91e57df0e4a83cabb5309ee1cd0512414d15847a
beckythebotato <beckyrderrick@gmail.com>
2021-10-01 12:04:33 +0100

    Pawns cant move when blocked and only two spaces at start

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -16,19 +16,47 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         ArrayList<Move> moves = new ArrayList<>();
-        Move whitePawnUp = new Move(from, from.plus(-1, 0));
-        Move blackPawnDown = new Move(from, from.plus(1, 0));
-        Move whitePawnUpTwo = new Move(from, from.plus(-2, 0));
-        Move blackPawnDownTwo = new Move(from, from.plus(2, 0));
+        int direction = 1;
+        int initialRow = 1;
 
-        moves.add(whitePawnUp);
-        moves.add(blackPawnDown);
-        if (from.getRow() == 6) {
-            moves.add(whitePawnUpTwo);
+        if (this.getColour() == PlayerColour.WHITE) {
+            direction = -1;
+            initialRow = 6;
         }
-        if (from.getRow() == 1) {
-            moves.add(blackPawnDownTwo);
+
+        Coordinates oneSpaceForward = from.plus(direction, 0);
+        Coordinates twoSpacesForward = from.plus(2 * direction, 0);
+
+        if (board.get(oneSpaceForward) == null) {
+            moves.add(new Move(from, oneSpaceForward));
         }
+
+        if (board.get(twoSpacesForward) == null && from.getRow() == initialRow) {
+            moves.add(new Move(from, twoSpacesForward));
+        }
+
         return moves;
+
+
+
+//
+//        Move whitePawnUp = new Move(from, from.plus(-1, 0));
+//        Move blackPawnDown = new Move(from, from.plus(1, 0));
+//        Move whitePawnUpTwo = new Move(from, from.plus(-2, 0));
+//        Move blackPawnDownTwo = new Move(from, from.plus(2, 0));
+//
+//        Coordinates to = from.plus(-2, 0);
+//
+//        moves.add(whitePawnUp);
+//        moves.add(blackPawnDown);
+//        if (from.getRow() == 6 && board.get(to) == null) {
+//
+//            moves.add(whitePawnUpTwo);
+//        }
+//        if (from.getRow() == 1) {
+//            moves.add(blackPawnDownTwo);
+//        }
+//
+//        return moves;
     }
 }
